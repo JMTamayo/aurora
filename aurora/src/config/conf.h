f690bf202d7e5334d2953c67847cd90889cce0a2
JMTamayo <jmtamayog23@gmail.com>
2025-08-01 21:52:16 -0500

    aurora: Implement configuration parameters for LED

--- aurora/src/config/conf.h
+++ aurora/src/config/conf.h
@@ -6,4 +6,13 @@ namespace config {
 extern const unsigned long
     DEFAULT_BAUD_RATE; // Default baud rate for serial communication
 
+// LED Configuration parameters:
+extern const unsigned int LED_PIN; // LED pin
+
+extern const unsigned int LED_CHANNEL; // LED channel
+
+extern const unsigned int LED_FREQUENCY; // LED frequency
+
+extern const unsigned int LED_RESOLUTION; // LED resolution
+
 } // namespace config
