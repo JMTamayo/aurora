b9ff2ff67250518263d255c85c2600bc6f9eec7a
JMTamayo <jmtamayog23@gmail.com>
2025-08-01 19:22:27 -0500

    aurora: Implement config module to handle the configuration parameters for the project

---
+++ aurora/src/config/conf.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace config {
+
+// Serial Configuration parameters:
+extern const unsigned long
+    DEFAULT_BAUD_RATE; // Default baud rate for serial communication
+
+} // namespace config
